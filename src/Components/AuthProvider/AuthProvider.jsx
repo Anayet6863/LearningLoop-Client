932518ae7c9ef4fa858db4e4bb48496455e42cd3
Md Anayet Ullah <109759412+Anayet6863@users.noreply.github.com>
2024-12-21 23:32:19 +0600

    Firbase is added.

---
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,75 @@
+import { Children, createContext, useEffect, useState } from "react";
+export const AuthContext = createContext();
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  updateProfile,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { auth } from "../../Firebase/Firebase.onfig";
+import { useNavigate } from "react-router-dom";
+
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
+  
+  // State for managing firebase:
+  const [user, setUser] = useState(null);
+  //console.log(user);
+  const provider = new GoogleAuthProvider();
+  // FirebaseAuthentication:
+  const handleLogOut = () => {
+    return signOut(auth);
+  };
+  const handleRegister = (email, password) => {
+     //console.log(email,password);
+    return createUserWithEmailAndPassword(auth,email,password)
+  };
+  const handleLogin = (email, password) => {
+     console.log(email,password);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const handleGoogleLogin =async () => {
+    return await signInWithPopup(auth, provider);
+  };
+  const manageProfile = (name, photo) => {
+    console.log(name, photo);
+   return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+const handleResetPassword = (email)=>{
+ return sendPasswordResetEmail(auth, email)
+}
+  const AuthInfo = {
+    handleLogin,
+    handleGoogleLogin,
+    handleLogOut,
+    handleRegister,
+    user,
+    manageProfile,
+    loading,
+    handleResetPassword,
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      return () => {
+        unsubscribe();
+      };
+    });
+  }, []);
+  return (
+    <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Components/Router/Router'
+import AuthProvider from './Components/AuthProvider/AuthProvider'
 
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
-  </RouterProvider>
+ <AuthProvider>
+   <RouterProvider router={router}>
+   </RouterProvider>
+ </AuthProvider>
 )
